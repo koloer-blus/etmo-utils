58cbf6e9a3323fa2e78b745acecce99ee20332b5
baiziyu-fe <baiziyu-fe@outlook.com>
2022-06-28 16:58:33 +0800

    fix(handler): this is not find

--- src/handler.ts
+++ src/handler.ts
@@ -3,21 +3,28 @@ import { ErrorHandler, FinalHandler } from './types';
 const defaultErrorHandler: ErrorHandler = (e) => console.error(e);
 const defaultFinalHandler: FinalHandler = () => console.log(`%c finish ${new Date()}`, 'color:blue');
 
-const Handle = function (successHandler: (p: any) => void, errorHandler?: ErrorHandler, finalHandler?: FinalHandler) {
-  this.successHandler = successHandler;
-  this.errorHandler = errorHandler || defaultErrorHandler;
-  this.finalHandler = finalHandler || defaultFinalHandler;
-};
+export class Handler {
+  successHandler: (p: any) => void;
+  errorHandler: ErrorHandler;
+  finalHandler: FinalHandler;
+  constructor(
+    successHandler: (p: any) => void,
+    errorHandler?: ErrorHandler,
+    finalHandler?: FinalHandler
+  ) {
+    this.successHandler = successHandler;
+    this.errorHandler = errorHandler || defaultErrorHandler;
+    this.finalHandler = finalHandler || defaultFinalHandler;
+  };
 
-Handle.prototype.use = function (callback: Function) {
-  try {
-    const res = callback();
-    this.successHandler(res);
-  } catch (e) {
-    this.errorHandler(e);
-  } finally {
-    this.finalHandler();
-  }
-};
-
-export const Handler = Handle;
+  use(callback: Function) {
+    try {
+      const res = callback();
+      this.successHandler(res);
+    } catch (e) {
+      this.errorHandler(e);
+    } finally {
+      this.finalHandler();
+    }
+  };
+}
